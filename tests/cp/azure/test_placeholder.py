6f5f78e68f6860b90d42808309cf3cea35daa8f7
Anthony Poddubny <anton.p@qualisystems.com>
2020-09-16 11:42:23 +0300

    add an empty test

--- tests/cp/azure/test_placeholder.py
+++ tests/cp/azure/test_placeholder.py
@@ -1,9 +1,11 @@
 import unittest
 
+from cloudshell.cp.azure import constants  # noqa: F401
+
 
 class TestEmpty(unittest.TestCase):
     def setUp(self):
         pass
 
     def test_smth(self):
-        pass
+        self.assertTrue(True)
