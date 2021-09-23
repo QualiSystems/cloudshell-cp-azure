101d6b821bcff7e44cf49fdc4a5e4f4c4ae8388c
Anthony Poddubny <Anton.p@qualisystems.com>
2021-09-23 11:07:25 +0300

    update setup.py file

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
-import os
-
 from setuptools import find_packages, setup
 
-with open(os.path.join("version.txt")) as version_file:
-    version_from_file = version_file.read().strip()
 
-with open("requirements.txt") as f_required:
-    required = f_required.read().splitlines()
+def read_file(file_name: str) -> str:
+    with open(file_name) as fo:
+        return fo.read().strip()
 
-with open("test_requirements.txt") as f_tests:
-    required_for_tests = f_tests.read().splitlines()
 
 setup(
     name="cloudshell-cp-azure",
     url="http://www.qualisystems.com/",
-    author="Quali",
-    author_email="info@quali.com",
+    author="QualiSystems",
+    author_email="info@qualisystems.com",
     packages=find_packages(),
-    install_requires=required,
-    tests_require=required_for_tests,
-    test_suite="nose.collector",
-    version=version_from_file,
+    install_requires=read_file("requirements.txt"),
+    tests_require=read_file("test_requirements.txt"),
+    python_requires="~=3.7",
+    version=read_file("version.txt"),
+    package_data={"": ["*.txt"]},
     description="",
     include_package_data=True,
 )
